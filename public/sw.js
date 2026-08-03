87e301847a4fc74217513f2f66289f9e1a2d676c
sonyadriko <sonyadiadriko@gmail.com>
2026-08-03 14:52:46 +0700

    release: bump v0.13.0 (kosakata English CEFR B1–C2, 300 kata)

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "levio-shell-v5";
+const CACHE_NAME = "levio-shell-v6";
 const PRECACHE_URLS = ["/", "/manifest.webmanifest", "/icon-192.png", "/icon-512.png"];
 const PRECACHE_PATHNAMES = PRECACHE_URLS.map(
   (url) => new URL(url, self.location.origin).pathname,
